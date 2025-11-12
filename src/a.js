a65ba9c1418de223e9d54d2213a747e2f1eb87b6
Roman <margelov322@gmail.com>
2025-11-13 00:05:51 +0300

    depluy

--- src/a.js
+++
@@ -1,4 +0,0 @@
-console.log('module a')
-
-import collect from 'collect.js'
-console.log(collect([1, 20, 40, 20]).avg())
--- src/main.js
+++
@@ -1,4 +0,0 @@
-import './assets/js/swiper-slide.js'
-import './assets/js/catalog-tabs.js'
-import './assets/js/modal-forms.js'
-import './assets/js/submit-forms.js'
